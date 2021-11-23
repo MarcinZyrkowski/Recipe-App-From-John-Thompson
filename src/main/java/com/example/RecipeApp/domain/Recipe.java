d2f82e28c6a2088d2063408f03dc45243b6bd3a2
mzyrkowski <mzyrkowski@griddynamics.com>
2021-11-23 11:32:57 +0100

    nc 2

--- src/main/java/com/example/RecipeApp/domain/Recipe.java
+++ src/main/java/com/example/RecipeApp/domain/Recipe.java
@@ -3,6 +3,7 @@ package com.example.RecipeApp.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
@@ -19,6 +20,8 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
     @Lob
     private Byte[] image;
@@ -26,7 +29,7 @@ public class Recipe {
     private Notes notes;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
@@ -35,8 +38,7 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
-
     )
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
 }
